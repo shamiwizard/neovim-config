4335857e8a8ab4dba4aaefe3c184c86476aeadf8
shamiwizard <leibyuk04mi@gmail.com>
2025-09-26 00:23:03 +0300

    Initial commit

---
+++ lua/shamiwizard/init.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " " -- Set leader key before Lazy
+ 
+require("shamiwizard.lazy_init")
+
+
+vim.cmd.colorscheme("tokyonight")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
