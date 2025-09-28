420aca2c4147d5494657d3ece261c6727afd586d
Mykhailo Leibiuk <leibyuk04mi@gmail.com>
2025-09-29 00:23:14 +0300

    Add neo-three, update config.

--- lua/shamiwizard/init.lua
+++ lua/shamiwizard/init.lua
@@ -10,12 +10,15 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set("n", "<leader>e", "<Cmd>Neotree<CR>")
 
 
+vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
 
