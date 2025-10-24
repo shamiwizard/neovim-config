9e0919ae8079fe4d91d9a4c6706ddfffc8e5953a
Mykhailo Leibiuk <leibyuk04mi@gmail.com>
2025-10-24 23:43:22 +0300

    Add colorcolumn

--- lua/shamiwizard/init.lua
+++ lua/shamiwizard/init.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "<leader>e", "<Cmd>Neotree<CR>")
 
 vim.opt.number = true
 vim.opt.tabstop = 2
+vim.opt.colorcolumn = '120'
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
